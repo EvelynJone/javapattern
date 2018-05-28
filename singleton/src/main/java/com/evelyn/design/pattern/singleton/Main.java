ec523f2b7fcf1717d4445c112073bf6d9a67eccc
waner <waner@tsign.cn>
2018-05-28 13:34:39 +0800

    单例模式

---
+++ singleton/src/main/java/com/evelyn/design/pattern/singleton/Main.java
@@ -0,0 +1,20 @@
+package com.evelyn.design.pattern.singleton;
+
+/**
+ * 功能说明：TODO
+ *
+ * @auther by zhaoxl
+ * @return <br/>
+ * 修改历史：<br/>
+ * 1.[2018年05月28日上午13:28]
+ */
+public class Main {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        if (s1 == s2) {
+            System.out.println("equals");
+        }
+    }
+}
---
+++ singleton/src/main/java/com/evelyn/design/pattern/singleton/Singleton.java
@@ -0,0 +1,26 @@
+package com.evelyn.design.pattern.singleton;
+
+/**
+ * 功能说明：Double-Check
+ *
+ * @auther by zhaoxl
+ * @return <br/>
+ * 修改历史：<br/>
+ * 1.[2018年05月28日上午13:27]
+ */
+public class Singleton {
+    private static volatile Singleton instance;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (instance) {
+                instance = new Singleton();
+            }
+        }
+        return instance;
+    }
+}
